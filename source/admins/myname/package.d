0821a723c6c13e6ffaa100cd745fd22aeb64bbe9
UIMSolutions <ui.manufaktur@gmail.com>
2023-06-26 12:04:36 +0200

    Update

--- source/admins/myname/package.d
+++ source/admins/myname/package.d
@@ -1,12 +1,43 @@
 module admins.myname;
 
-public { // required uim libraries
+mixin(ImportPhobos!());
+
+// Dub
+public import vibe.d;
+
+// UIM
+public {
   import uim.core;
+  import uim.bootstrap;
+  import uim.html;
   import uim.oop;
-  import uim.admins;
+  import uim.models;
+  import uim.apps;
+  /* import uim.web; */
+  import uim.admin;
+}
+
+public {
+  import web.controls;
+}
+
+public {
+  import langs.javascript;
 }
 
 public { // admin-myname libraries
   import admins.myname.controllers;
+  import admins.myname.helpers;
+  import admins.myname.routes;
+  import admins.myname.tests;
   import admins.myname.views;
+}
+
+@safe:
+static this() { // Create and init app
+  auto myAdmin = new class DAdmin {
+    override void initialize(Json configSettings = Json(null)) {
+      super.initialize(configSettings);
+    }
+  }
 }
